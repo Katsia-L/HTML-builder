65f38cd7f30858b19a5c0a9bd5421167200d970a
Katsia-L <119506222+Katsia-L@users.noreply.github.com>
2025-01-21 13:33:27 +0100

    fix: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,8 +11,10 @@ async function secretFolder() {
       const fileFolder = path.join(filePath, file.name);
       const fileSize = await fs.stat(fileFolder);
 
-      const name = file.name.split('.')[0];
-      const type = file.name.split('.')[1];
+      const nameParts = file.name.split('.');
+
+      const name = nameParts.slice(0, -1).join('.');
+      const type = nameParts[nameParts.length - 1];
       const size = Math.floor((fileSize.size / 1024) * 1000) / 1000;
 
       console.log(`${name} - ${type} - ${size}kb`);
